b6724107b9d68eef515483fff3d912ddc61cc4ee
Kostya <krot.kostya.krot@gmail.comm>
2022-05-14 14:48:42 +0300

    Updates

---
+++ precache-manifest.394cfbba197d23d596dc2c8cda48c72e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e50deedbeb7c1aa64a5336925a97a85",
+    "url": "/MySoc/index.html"
+  },
+  {
+    "revision": "53de0d27877242401c47",
+    "url": "/MySoc/static/css/main.68a9a6e4.chunk.css"
+  },
+  {
+    "revision": "31cc821e65dfb865527f",
+    "url": "/MySoc/static/js/2.cbc9c74e.chunk.js"
+  },
+  {
+    "revision": "53de0d27877242401c47",
+    "url": "/MySoc/static/js/main.8dbe1120.chunk.js"
+  },
+  {
+    "revision": "4bbf896913222b25426d",
+    "url": "/MySoc/static/js/runtime-main.8da1d4b9.js"
+  }
+]);
